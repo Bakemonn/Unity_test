451f6290e6037e1b8058c7832bff3b35ba4ad3ca
Bakemonn <mbakgminc@gmail.com>
2017-05-27 23:58:54 +0900

    First Commit
    test

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public float Speed = 15.0f;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonUp("Jump") && rb.velocity == new Vector3(0, 0, 0))
+        {
+            rb.AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
+        }
+	}
+}
